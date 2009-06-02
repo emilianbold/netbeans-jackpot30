c6dad3862925535b01d83d99f8bdaaa1e2c12cb9
Jan Lahoda <jlahoda@netbeans.org>
2009-06-02 16:36:29 +0200

    Adding double checked locking hint.

---
+++ hintsimpl/test/unit/src/org/netbeans/modules/jackpot30/hintsimpl/HintsTest.java
@@ -0,0 +1,16 @@
+package org.netbeans.modules.jackpot30.hintsimpl;
+
+import junit.framework.TestSuite;
+import org.netbeans.modules.jackpot30.file.test.spi.DeclarativeHintsTestBase;
+
+/**
+ *
+ * @author lahvac
+ */
+public class HintsTest extends DeclarativeHintsTestBase {
+
+    public static TestSuite suite() {
+        return suite(HintsTest.class);
+    }
+
+}
