80f8ca0eebb5854c406b9270123d39798d91b83e
Jan Lahoda <jlahoda@netbeans.org>
2009-06-18 13:42:28 +0200

    Adding panel to the FindDuplicatesRefactoring.

--- api/src/org/netbeans/modules/jackpot30/impl/refactoring/FindDuplicatesRefactoringUI.java
+++ api/src/org/netbeans/modules/jackpot30/impl/refactoring/FindDuplicatesRefactoringUI.java
@@ -1,5 +1,7 @@
 package org.netbeans.modules.jackpot30.impl.refactoring;
 
+import java.awt.Component;
+import java.util.Collections;
 import javax.swing.event.ChangeListener;
 import org.netbeans.modules.jackpot30.impl.batch.BatchSearch.Scope;
 import org.netbeans.modules.jackpot30.spi.HintDescription.PatternDescription;
@@ -11,8 +13,8 @@ import org.openide.util.HelpCtx;
 
 public class FindDuplicatesRefactoringUI implements RefactoringUI {
 
-    private final PatternDescription pattern;
-    private final Scope scope;
+    private volatile PatternDescription pattern;
+    private volatile Scope scope;
 
     public FindDuplicatesRefactoringUI(PatternDescription pattern, Scope scope) {
         this.pattern = pattern;
@@ -31,11 +33,26 @@ public class FindDuplicatesRefactoringUI implements RefactoringUI {
         return true;
     }
 
-    public CustomRefactoringPanel getPanel(ChangeListener parent) {
-        return null;
+    private FindDuplicatesRefactoringPanel panel;
+
+    public CustomRefactoringPanel getPanel(final ChangeListener parent) {
+        return new CustomRefactoringPanel() {
+            public void initialize() {}
+            public Component getComponent() {
+                if (panel == null) {
+                    panel = new FindDuplicatesRefactoringPanel(parent);
+                    panel.setPattern(pattern.getPattern());
+                    panel.setScope(scope);
+                }
+
+                return panel;
+            }
+        };
     }
 
     public Problem setParameters() {
+        pattern = PatternDescription.create(panel.getPattern(), Collections.<String, String>emptyMap());
+        scope   = panel.getScope();
         return null;
     }
 
@@ -44,7 +61,7 @@ public class FindDuplicatesRefactoringUI implements RefactoringUI {
     }
 
     public boolean hasParameters() {
-        return false;
+        return true;
     }
 
     public AbstractRefactoring getRefactoring() {
