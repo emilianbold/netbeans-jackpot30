b7c941fd083d953c592cd806b856b24e6153d4e6
Jan Lahoda <jlahoda@netbeans.org>
2011-06-23 20:33:22 +0200

    Correcting tool name

--- remoting/server/scripts/index.sh
+++ remoting/server/scripts/index.sh
@@ -9,6 +9,6 @@ NAME="$1"; shift
 TARGET="$1"; shift
 ROOT_DIR="$1"; shift
 
-$DIR/indexer/bin/backend --userdir $USERDIR --nosplash --nogui -J-Xmx2048m --category-id "$ID" --category-name "$NAME" --cache-target "$TARGET" --category-root-dir "$ROOT_DIR" --category-projects "$@"
+$DIR/indexer/bin/indexer --userdir $USERDIR --nosplash --nogui -J-Xmx2048m --category-id "$ID" --category-name "$NAME" --cache-target "$TARGET" --category-root-dir "$ROOT_DIR" --category-projects "$@"
 
 exit
