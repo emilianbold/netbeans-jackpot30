9ea05aed68909cbb8351cfb91e1b06bac55824de
Jan Lahoda <jlahoda@netbeans.org>
2011-07-07 20:11:36 +0200

    The remoting indices typically do not fit into the memory, disable the Lucene's index memory cache

--- remoting/server/scripts/web.sh
+++ remoting/server/scripts/web.sh
@@ -1,2 +1,2 @@
 DIR=`dirname $0`
-java -Xbootclasspath/p:$DIR/web/lib/javac-api-nb-7.0-b07.jar:$DIR/web/lib/javac-impl-nb-7.0-b07.jar -jar $DIR/web/web.main.jar "$@"
+java -Djava.index.useMemCache=false -Xbootclasspath/p:$DIR/web/lib/javac-api-nb-7.0-b07.jar:$DIR/web/lib/javac-impl-nb-7.0-b07.jar -jar $DIR/web/web.main.jar "$@"
