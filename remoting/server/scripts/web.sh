3fc0fabfa4361ff2030181b23e00780a9b648c4d
Jan Lahoda <jlahoda@netbeans.org>
2012-05-04 21:39:23 +0200

    javac-{api,impl}-nb-... renamed to nb-javac-{api,impl}, adjusting

--- remoting/server/scripts/web.sh
+++ remoting/server/scripts/web.sh
@@ -1,2 +1,2 @@
 DIR=`dirname $0`
-java -Djava.index.useMemCache=false -Xbootclasspath/p:$DIR/web/lib/javac-api-nb-7.0-b07.jar:$DIR/web/lib/javac-impl-nb-7.0-b07.jar -jar $DIR/web/web.main.jar "$@"
+java -Djava.index.useMemCache=false -Xbootclasspath/p:$DIR/web/lib/nb-javac-api.jar:$DIR/web/lib/nb-javac-impl.jar -jar $DIR/web/web.main.jar "$@"
