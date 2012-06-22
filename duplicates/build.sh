fbb6bec96d2bf550bb2f284bf94e4ef332ef2a55
Jan Lahoda <jlahoda@netbeans.org>
2012-06-22 23:15:13 +0200

    Server-side indexing for duplicates

--- duplicates/build.sh
+++ duplicates/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 (cd ide; ant "$@" clean && ant "$@" nbms && ant "$@" test) || exit 1
+(cd server/indexer; ant "$@" clean && ant "$@" build && cp -r build/cluster build/indexer; cp -r ../../ide/build/cluster/* build/indexer/; cp -r ../../../remoting/ide/build/cluster/* build/indexer/; cd build; zip -r ../../../../remoting/build/duplicates-indexer.zip indexer/) || exit 1
 
