65a4025c75a2f49c73983888957b264e4f27cd74
Jan Lahoda <jlahoda@netbeans.org>
2012-07-11 07:13:46 +0200

    Need to actually build the duplicates web frontend

--- duplicates/build.sh
+++ duplicates/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 (cd ide; ant "$@" clean && ant "$@" nbms && ant "$@" test) || exit 1
 (cd server/indexer; ant "$@" clean && ant "$@" build && cp -r build/cluster build/indexer; cp -r ../../ide/build/cluster/* build/indexer/; cp -r ../../../remoting/ide/build/cluster/* build/indexer/; cd build; zip -r ../../../../remoting/build/duplicates-indexer.zip indexer/) || exit 1
+(cd server/web/duplicates.web.api; ant "$@" clean && ant "$@") || exit 1
 
