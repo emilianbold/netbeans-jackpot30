9f5df8c59297cb1b36d93062e311dc4b56d6c106
Jan Lahoda <jlahoda@netbeans.org>
2012-07-16 21:08:25 +0200

    Need to include the language front end in the indexing backend

--- remoting/post-build.sh
+++ remoting/post-build.sh
@@ -3,7 +3,8 @@ cp -r ../duplicates/server/indexer/build/cluster/* build/indexing-backend/indexe
 cp -r ../duplicates/ide/build/cluster/* build/indexing-backend/indexer/indexer
 cp -r ../remoting/ide/build/cluster/* build/indexing-backend/indexer/indexer
 cp -r ../language/ide/build/cluster/* build/indexing-backend/indexer/indexer
-cp -r ../duplicates/server/web/duplicates.web.api/dist/*.jar build/indexing-backend/web/lib
+cp -r ../duplicates/server/web/*/dist/*.jar build/indexing-backend/web/lib
+cp -r ../language/server/web/*/dist/*.jar build/indexing-backend/web/lib
 
 (cd build; zip -r indexing-backend-feature-packed.zip indexing-backend) || exit 1
 (cd build; zip -r indexing-backend-feature-packed-shortened.zip `find indexing-backend -type f | grep -v indexing-backend/indexer/enterprise/ | grep -v indexing-backend/indexer/apisupport/  | grep -v indexing-backend/indexer/cnd/   | grep -v indexing-backend/indexer/dlight/   | grep -v indexing-backend/indexer/harness/   | grep -v indexing-backend/indexer/ide/   | grep -v indexing-backend/indexer/java   | grep -v indexing-backend/indexer/nb/   | grep -v indexing-backend/indexer/platform/   | grep -v indexing-backend/indexer/profiler/   | grep -v indexing-backend/indexer/websvccommon/`) || exit 1
