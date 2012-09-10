cc30ee7647e597ada8b38bdbe0254301ca7e2dbb
Jan Lahoda <jlahoda@netbeans.org>
2012-09-10 23:36:09 +0200

    Must deploy the tool jar to the 'remote' repository, not install to it

--- cmdline/build.sh
+++ cmdline/build.sh
@@ -4,6 +4,6 @@ mvn $MAVEN_EXTRA_ARGS install:install-file -Dfile=tool/build/jackpot/jackpot.jar
 (cd maven; mvn $MAVEN_EXTRA_ARGS install -DskipTests;  mvn $MAVEN_EXTRA_ARGS -Dmaven.executable=`which mvn` test)
 MAVEN_REPO=`pwd`/build/.m2
 mkdir -p "$MAVEN_REPO"
-mvn $MAVEN_EXTRA_ARGS install:install-file -Dfile=tool/build/jackpot/jackpot.jar -DgroupId=org.netbeans.modules.jackpot30 -DartifactId=tool -Dversion=7.2-SNAPSHOT -Dpackaging=jar -DgeneratePom=true "-DlocalRepositoryPath=$MAVEN_REPO"
+mvn $MAVEN_EXTRA_ARGS deploy:deploy-file -Dfile=tool/build/jackpot/jackpot.jar -DgroupId=org.netbeans.modules.jackpot30 -DartifactId=tool -Dversion=7.2-SNAPSHOT -Dpackaging=jar -DgeneratePom=true -DaltDeploymentRepository=temp::default::file://"$MAVEN_REPO" -Durl=file://"$MAVEN_REPO"
 (cd maven; mvn $MAVEN_EXTRA_ARGS -DskipTests -DaltDeploymentRepository=temp::default::file://"$MAVEN_REPO" deploy)
 (cd build; zip -r .m2.zip .m2)
