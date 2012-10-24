fe7318bc0252641dcafb5acdfb1aebea3c44071d
Jan Lahoda <jlahoda@netbeans.org>
2012-10-24 16:42:09 +0200

    Ability to specify command line arguments for the web frontend

--- remoting/server/scripts/web.sh
+++ remoting/server/scripts/web.sh
@@ -5,4 +5,4 @@ classpath="$DIR/web/web.main.jar"
 for jar in $DIR/web/lib/*.jar; do
     classpath="$classpath:$jar"
 done
-java -Djava.index.useMemCache=false -Xbootclasspath/p:$DIR/web/lib/nb-javac-api.jar:$DIR/web/lib/nb-javac-impl.jar -classpath "$classpath" web.main.WebMain "$@"
+java $JACKPOT_WEB_OPTS -Djava.index.useMemCache=false -Xbootclasspath/p:$DIR/web/lib/nb-javac-api.jar:$DIR/web/lib/nb-javac-impl.jar -classpath "$classpath" web.main.WebMain "$@"
